22b3759ab0164cfbe093204f2f0ecf9ecaa5c2f0
morimotojumpei <moriotojumpei9@gmail.com>
2021-02-14 01:26:15 +0900

    new file

---
+++ app_event/resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+@component('components.header')
+@endcomponent
+
+<main class="py-4 mb-5">
+  @yield('content')
+</main>
+
+@component('components.footer')
+@endcomponent
+
+</div>
+</body>
+</html>
---
+++ app_event/resources/views/welcome.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<!-- 照こくちーずプロ参 -->
+<html lang="ja">
+
+  <!-- SNS対策 先生に聞く-->
+  <!-- <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#"><script src="https://pagead2.googlesyndication.com/pagead/js/r20201201/r201100131/rum_fy2019.js"></script><script src="https://www.googletagservices.com/activeview/js/current/osd.js?cb=%2Fr20100101"></script><script src="https://partner.googleadservices.com/gampad/cookie.js?domain=www.kokuchpro.com&amp;callback=_gfp_s_&amp;client=ca-pub-5936792356450049&amp;cookie=ID%3D5ac6b1c9f6b566a9%3AT%3D1607006056%3AS%3DALNI_MbIIC-4b3eJULUE7LtkRzI5d_SKWQ"></script> -->
+
+  <!-- SNS情報 -->
+  <meta property="fb:app_id" content="あああああああ">
+  <meta property="og:type" content="あああああ">
+  <meta property="og:url" content="ああああああ">
+  <meta property="og:title" content="ああああああああ">
+  <meta property="og:image" content="あああああああああ">
+
+  <!-- 以下は必要？
+  <meta name="twitter:site" content="ああああああああ">
+  <meta name="twitter:card" content="ああああああああ"> -->
+
+  <!-- 新しいIEに変更してねって意味←不要かも！！-->
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
+  <meta charset="utf-8">
+
+  <!-- レスポンス対応 -->
+  <!-- <meta name="viewport" content="width=1030px,maximum-scale=2.0,user-scalable=yes"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- name="description"は、その文書の説明を短文で指定します。 ここで指定する値は、Googleの検索結果に表示されるなど、多くの検索エンジンで利用されるので慎重に言葉を選んでください。 -->
+  <meta name="Description" content="梅田、関西、イベント、2021年、飲み会、Mikodapps">
+
+  <!-- SEO対策 先生にきく-->
+  <!-- <link rel="alternate" type="application/rss+xml" title="神戸飲み会イベント特集 - こくちーずプロ" ?href="https://www.kokuchpro.com/feature/%E7%A5%9E%E6%88%B8%E9%A3%B2%E3%81%BF%E4%BC%9A/.rss"> -->
+
+  <meta name="Key word" content=”Mikodapps,イベント,セミナー,勉強会,コミュニティ,集客,告知,チケット,主催,無料,ソーシャル,梅田,関西,イベント,2021年,飲み会”>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/index/reset.css">
+  <link rel="stylesheet" href="css/index/header.css">
+  <link rel="stylesheet" href="css/index/main.css">
+  <link rel="stylesheet" href="css/index/footer.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  <link rel="stylesheet" href="fullcalendar/main.css" />
+
+  <!-- --font-awsome-- -->
+  <script src="https://kit.fontawesome.com/720c0d4f32.js" crossorigin="anonymous"></script>
+
+    <!-- Google Tag Manager -->
+
+    <!-- Minimize page flickering -->
+
+    <!-- Google Analytics Tracking code -->
+
+    <!-- DFP Tracking code -->
+
+    <!-- Favicons -->
+    <link rel=”icon” href=“/image/favicon/favicon.ico”>
+    <title>Mikodapps_飲み会イベント</title>
+</head>
+
+<body>
+  <header>
+    <div class="top_category">
+      <div class="left">
+        <h1>Mikodapps <span style="font-size:10px">いつもここから</span></h1>
+      </div>
+      <div class="right">
+        <p>お気軽にお問い合わせください。</p>
+        <p>TEL:080-xxxx-xxxx</p>
+      </div>
+    </div>
+    <div class="header_image">
+        <div class="slider">
+          <img src="image/image1.png" height="400" alt="">
+          <img src="image/image2.png" height="400" alt="">
+          <img src="image/image3.png" height="400" alt="">
+          <img src="image/image4.png" height="400" alt="">
+        </div>
+      <ul class="content" >
+        <li class="menu1"><a href="/app_event/public/">Home</a></li>
+        <li class="menu2"><a href="schedule">Schedule</a></li>
+        <li class="menu3"><a href="info">Info</a></li>
+        <li class="menu4"><a href="first_time">First time</a></li>
+        <li class="menu5"><a href="question">Question</a></li>
+        <li class="menu6"><a href="contact">Contact</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <main>
+    <diV>
+    <p>Mikodappsの活動内容</p>
+    <p>テキスと</p><br>
+    <p>イベントスケジュール</p>
+    <p>クリックできるようにする</p><br>
+    <p>過去のイベント</p>
+    <p>動画入れる</p>
+
+    <p>参加者のコメント</p>
+    <p>利用規約</p>
+  </div>
+
+    
+
+<footer>
+  <!-- 背景が正方形 -->
+  <ul class="pattern-03 square">
+    <li>
+        <a href="#" class="icon-wrap icon-wrap-instagram">
+            <i class="fab fa-instagram fa-3x"></i>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="icon-wrap icon-wrap-twitter">
+            <i class="fab fa-twitter fa-3x"></i>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="icon-wrap icon-wrap-line">
+            <i class="fab fa-line fa-3x"></i>
+        </a>
+    </li>
+  </ul>
+      <p class="copyr" data-copyr="">Copyright© Mikoフェスタ,  2020 All Rights Reserved.</p>
+    </div>
+  </div>
+</footer>
+
+<!-- jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- スライダー -->
+  <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<!-- カレンダー -->
+<script src="fullcalendar/main.js"></script>
+
+<script>
+  $(document).ready(function(){
+    $('.slider').bxSlider({
+      pager: false,
+      controls: false,
+      auto: true,
+      pause: 4000,
+    });
+  });
+</script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth', // オプション （月表示）
+      firstDay: 1, //0なら日曜日始まり
+      events: [
+        {
+          id: 'a',
+          title: 'GODBOX争奪戦',
+          start: '2021-01-30',
+          url: 'http://google.com/'
+        },
+        {
+          id: 'b',
+          title: '広島',
+          start: '2021-02-06',
+          url:'https://www.yahoo.co.jp/'
+        }
+      ],
+
+    eventClick: function(info) {
+      info.jsEvent.preventDefault();
+      if (info.event.url) {
+        window.open(info.event.url);
+        }
+    }
+    });
+    calendar.render(); // カレンダーの初期化（再レンダリング）
+  });
+</script>
+
+<!-- スクロールアニメーション -->
+<script>
+const list = document.querySelectorAll(".list");
+console.log("画面の高さ" , window.innerHeight);
+document.addEventListener("scroll" , function(){
+  for (let i = 0; i < list.length; i++) {
+  const getList = list[i].
+  getBoundingClientRect().top + list[i].clientHeight*0.3
+
+  // getBoundingClientRect().top 今見えているブラスザTOPからの距離
+  // clientHeight sectionの高さ。６割見えたら
+  // window.innerHeight 現在のブラウザの高さ
+    if(window.innerHeight > getList) {
+      list[i].classList.add("show");
+    }
+  }
+})
+
+</script>
+
+</body>
+</html>
+
